b7bd47688ab45e8a74edcbaea2978d048082d94e
Ethan Witwer <ethan@ethanwitwer.com>
2026-05-04 11:21:51 +0200

    Adding home page and license notice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
+// Required Notice: Copyright 2026 Ethan Witwer (https://dodge.video)
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 
